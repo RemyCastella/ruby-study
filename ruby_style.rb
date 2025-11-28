1ca7e61c0c94e590fe73fa9a5d8364ed59eef2fd
r-castella@ruby-dev.jp <r-castella@ruby-dev.jp>
2025-11-28 10:55:08 +0900

    ruby style notes

---
+++ ruby_style.rb
@@ -0,0 +1,12 @@
+# consistency > perfection
+# style guides
+https://rubystyle.guide/
+https://ruby-style-guide.shopify.dev/
+# Standard - a default RuboCop config
+# Just add gem standard, no additional config needed
+# run with standardrb
+# extend_config: rails_cops.yml
+# Duck typing: typing based on messages an object responds to
+
+# to_s vs to_str (the latter is stricter.)
+# if an object implements to_str, it can be used everywhere a string can be used
